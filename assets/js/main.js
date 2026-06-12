df59e080abec482cbc31689daa1eea55a003be3b
BINH LE <lehaduybinh28@gmail.com>
2026-06-12 18:56:28 -0400

    update countdown timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -299,7 +299,7 @@
 		=        countdown	   =
 	=============================================*/
     function makeTimer() {
-		var endTime = new Date("20 nov 2026 9:56:00 GMT+01:00");
+		var endTime = new Date("20 jun 2026 17:00:00 GMT-04:00");
 		endTime = (Date.parse(endTime) / 1000);
 		var now = new Date();
 		now = (Date.parse(now) / 1000);
